8c76b3e83fba6fd85e2e0b5d4928b84fa040e84b
Gregory Casamento <greg.casamento@gmail.com>
2020-11-16 16:36:05 -0500

    Process line by line

--- nib_count.rb
+++ nib_count.rb
@@ -18,15 +18,34 @@ class NibCount
     regexp = /^\t\t<[a-zA-Z]/
     n = 0
     @data.each do |line|
+      found_end = false
       i = regexp.match( line )
       if i.nil? == false
-        line.insert(1, n.to_s) unless i.nil?
+        if n.to_s.length > 3
+          line.insert(0, n.to_s) unless i.nil?
+          line = line.gsub( "\t\t", "\t" )
+        else
+          line.insert(1, n.to_s) unless i.nil?
+        end
         n = n + 1
       end
       result = result + line
     end
     result
   end
+
+  def write_enum_nib
+    f = File.open("/Users/heron/Desktop/MainMenu-enumerated.nib","w")
+    f.write(@result)
+    f.close
+  end
+
+  def execute
+    read_nib
+    @result = enumerate_objects
+    write_enum_nib
+  end
+
 
   def initialize
     super
@@ -35,6 +54,4 @@ class NibCount
 end
 
 nc = NibCount.new
-nc.read_nib
-result = nc.enumerate_objects
-puts result
+nc.execute
